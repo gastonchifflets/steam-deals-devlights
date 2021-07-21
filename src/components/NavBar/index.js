5b26b6c3035922c8b9a6711d710c3d90b8a9a4ae
gastonchifflets <gastonchifflets@gmail.com>
2021-07-21 16:44:55 -0300

    NavBar component added

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+
+import { Icon } from '@iconify/react';
+import shoppingCart from '@iconify-icons/fa-solid/shopping-cart';
+
+import logo from '../../img/logo.png';
+import './index.css';
+
+export default function NavBar () {
+    return (
+    <nav>
+        <div className='navbar container'>
+            <img src={logo} alt='logo'/>
+            <ul className='navbar-items'>
+                <li>
+                    <Link to='/' className='navbar-item'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/browse' className='navbar-item'>Browse</Link>
+                </li>
+                <li>
+                    <Link to='/cart' className='navbar-item secondary'>
+                        <Icon icon={shoppingCart}/>
+                        <span className='item-text'>Cart</span>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    );
+}
---
+++ src/views/Home/index.js
@@ -0,0 +1,11 @@
+import NavBar from "../../components/NavBar";
+
+import './index.css';
+
+export default function Home() {
+    return (
+        <div className=''>
+            <NavBar />
+        </div>
+    );
+}
